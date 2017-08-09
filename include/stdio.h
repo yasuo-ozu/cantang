1532f3102183131e27c07742a82ac356ddf07c3b
yozu <yasuo@ozu.email>
2017-08-09 09:07:33 +0900

    Added stdio.h

---
+++ include/stdio.h
@@ -0,0 +1,48 @@
+int stdin = 0, stdout = 1, stderr = 2;
+
+int printf(char *format, int val) {
+	return fprintf(stdout, format, val);
+}
+
+int putint(int fd, int val) {
+	char *s = "s";
+	int i = 1;
+	while (val / i > 0) i = i * 10;
+	if (i > 1) i = i / 10;
+	while (i) {
+		s[0] = (val / i) + 48;	// 48 == '0'
+		puts s;
+		val = val % i;
+		i = i / 10;
+	}
+}
+
+int fprintf(int fd, char *format, int val) {
+	char *s = "S", c;
+	int i;
+	for (i = 0; 1; i++) {
+		c = format[i];
+		if (c == 0) break;
+		if (c == 37) {	// '%'
+			i++;
+			c = format[i];
+			if (c == 115) {	// 's'
+				puts val;
+			} else {
+				if (c == 108) { // 'l'
+					i++;
+					c = format[i];
+					print c;
+					if (c == 108) i++;
+				}
+				putint(fd, val);
+			}
+			continue;
+		}
+		// TODO: *s = c とすると sが変化してしまう
+		s[0] = c;
+		puts s;
+	}
+}
+
+fprintf(stdout, "hello, %lldworld", 1234);
