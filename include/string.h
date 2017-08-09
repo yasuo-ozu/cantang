c815f7307f3edcdde36563e722be21505853f7fb
yozu <yasuo@ozu.email>
2017-08-09 16:11:14 +0900

    Fixed string.h

--- include/string.h
+++ include/string.h
@@ -1,22 +1,3 @@
-int malloc(int size) {
-	int a[size];
-	return a;
-}
-
-int calloc(int count, int size) {
-	size = count * size;
-	int *a = malloc(size);
-	int i;
-	for (i = 0; i < size; i++) a[i] = 0;
-	return a;
-}
-
-int memcpy(char *dest, char *src, int size) {
-	int i;
-	for (i = 0; i < size; i++) {
-		dest[i] = src[i];
-	}
-}
 int strlen(char *s) {
 	int i;
 	for (i = 0; s[i] != 0; i++);
